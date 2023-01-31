b335bdd7c41efe2c8910eedbb0ae5a7cb8f46ce8
Harishrao <117077716+harishrao1@users.noreply.github.com>
2023-01-31 19:28:16 +0530

    Create Pyramid.java

---
+++ Pyramid.java
@@ -0,0 +1,12 @@
+public class Invertedpyramid {
+    public static void main(String[] args) {
+        Scanner sc = Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<=n;i--){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
