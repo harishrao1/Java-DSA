1adf13cd3d642eb50ec93d5610805c162f719626
Harishrao <117077716+harishrao1@users.noreply.github.com>
2023-01-31 20:05:47 +0530

    Update main.java

--- LeetCode/TwoSum/main.java
+++ LeetCode/TwoSum/main.java
@@ -1,1 +1,22 @@
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0; i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]+nums[j]== target){
+                    if(i!=j){
+                        return new int[]{i,j};
+                    }
+                }
+            }
+        }
+        return null;
+        
+    }
+}
+
+// nums=[1,2,3,4,7]
+
+// target = 9
+
+// output =[1,4]
