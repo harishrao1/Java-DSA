fcd5bd346d5e29e5a31b573ddb8271b9555dfcd5
Harishrao <117077716+harishrao1@users.noreply.github.com>
2023-02-05 19:26:16 +0530

    Update main.java

--- Arrays/ContainsDuplicates/main.java
+++ Arrays/ContainsDuplicates/main.java
@@ -8,7 +8,7 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         
         Arrays.sort(nums);
-        int ans = 0;
+      
         for(int i =0;i<nums.length-1;i++){
             
             if(nums[i] == nums[i+1]){
