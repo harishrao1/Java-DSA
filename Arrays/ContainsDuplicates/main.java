75bf0feaaab256ed15a77e8e88395515b8366439
Harishrao <117077716+harishrao1@users.noreply.github.com>
2023-02-05 19:25:38 +0530

    Create main.java

---
+++ Arrays/ContainsDuplicates/main.java
@@ -0,0 +1,33 @@
+class Solution {
+  
+//   Input: nums = [1,2,3,1]
+//   Output: true
+    
+  
+//   using sort --> O(nlogn)
+    public boolean containsDuplicate(int[] nums) {
+        
+        Arrays.sort(nums);
+        int ans = 0;
+        for(int i =0;i<nums.length-1;i++){
+            
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+// iterative method
+// O(n^2)
+/*
+for(int i=0;i<nums.length;i++){
+  for(int j=i+1;j<nums.lengthlj++){
+    if(nums[j] == nums[i]){
+      return true;
+    }
+  }
+}
+return false;
+*/
